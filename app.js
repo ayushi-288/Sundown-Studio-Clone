7f4551fc599a8d0d22fa91f0f4b444212981af63
Ayushi Kushwaha <147259686+ayushi-288@users.noreply.github.com>
2024-07-31 09:25:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('#main'),-
-//     smooth: true
-// });
+const scroll = new LocomotiveScroll({
+     el: document.querySelector('#main'),-
+     smooth: true
+});
 
 let elemC= document.querySelector("#elem-container");
 let fixed= document.querySelector("#fixed-image");
